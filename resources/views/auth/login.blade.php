9134ad121ea7335fa92ccce5eafd48a0f4b36ad2
RaelEstate <rhyllneri@gmail.com>
2024-04-25 20:19:24 +0800

    000000

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
         <!-- Email Address -->
         <div class="mb-4">
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full bg-transparent" type="email" name="email"
+            <x-text-input id="email" class="block mt-1 w-full bg-transparent px-3 py-2" type="email" name="email"
                 :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
@@ -17,20 +17,12 @@
         <div class="mb-4">
             <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full bg-transparent" type="password" name="password"
-                required autocomplete="current-password" />
+            <x-text-input id="password" class="block mt-1 w-full bg-transparent px-3 py-2" type="password"
+                name="password" required autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Remember Me -->
-        <div class="block mb-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox"
-                    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
 
         <div class="flex items-center justify-end">
             @if (Route::has('password.request'))
